081f0d2603e1b3a11484471318741cde73d732f4
Stannnnn <stann@live.nl>
2016-01-08 23:32:33 +0100

    Tekst save start

--- app/src/main/java/nl/fhict/sketchboard/layers/TextLayer.java
+++ app/src/main/java/nl/fhict/sketchboard/layers/TextLayer.java
@@ -4,6 +4,9 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PointF;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,10 +24,6 @@ public class TextLayer implements Layerable {
         this.paint = paint;
     }
 
-    public void setPaint(Paint paint) {
-        this.paint = paint;
-    }
-
     @Override
     public String getName() {
         return name;
@@ -33,5 +32,27 @@ public class TextLayer implements Layerable {
     @Override
     public void draw(Canvas canvas) {
         canvas.drawText(this.text, 10, 10, this.paint);
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.writeObject(name);
+        out.writeObject(text);
+
+        out.writeObject(paint.getAlpha());
+        out.writeObject(paint.getColor());
+        // Missing shit
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException{
+        this.name = (String) in.readObject();
+        this.text = (String) in.readObject();
+
+        int alpha = (int) in.readObject();
+        int color = (int) in.readObject();
+
+        this.paint = new Paint();
+        this.paint.setAlpha(alpha);
+        this.paint.setColor(color);
+        // Missing shit
     }
 }
